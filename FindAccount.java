2c13d606b042c10165696ad02b198aa5f127dc50
Nethaji <103849020+nethaji26@users.noreply.github.com>
2022-04-30 17:47:59 +0530

    Add files via upload

--- FindAccount.java
+++ FindAccount.java
@@ -1,13 +1,14 @@
 package week2.day2;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FindAccount {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
@@ -21,10 +22,12 @@ public class FindAccount {
 		driver.findElement(By.linkText("Find Accounts")).click();
 		driver.findElement(By.xpath("(//input[@name='accountName'])[2]")).sendKeys("Credit Limited Account");
 		driver.findElement(By.xpath("//button[contains(text(),'Find Accounts')]")).click();
-		driver.findElement(By.xpath("(//a[contains(text(),'accountlimit100')])[1]")).click();
+		Thread.sleep(5000);
+		WebElement element = driver.findElement(By.xpath("(//a[contains(text(),'accountlimit100')])[1]"));
+		element.click();
 		String text = driver.findElement(By.xpath("(//span[@class='tabletext'])[3]")).getText();
-		System.out.println(text);
-		if(text=="Credit Limited Account (accountlimit100)")
+		System.out.println("Account name is " + text);
+		if(text.contains("Credit Limited Account (accountlimit100)"))
 		{
 			System.out.println("pass");
 		}else
@@ -32,13 +35,16 @@ public class FindAccount {
 		}
 		String text1 = driver.findElement(By.xpath("(//td[@class='tabletext'])[1]")).getText();
 		System.out.println(text1);
-		if(driver.getTitle() =="Account Details")
+		String title = driver.getTitle();
+		System.out.println(title);
+		if(title.contains("opentaps"))
 		{
 			System.out.println("pass");}
 			else
 			{
 				System.out.println("mismatch");
 			}
+		
 		}
 		
 
